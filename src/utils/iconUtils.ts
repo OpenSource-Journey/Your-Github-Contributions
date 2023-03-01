ea48aaccf4498feed2156623d077f055fa6bdc5d
anuj-kumary <anujf0510@gmail.com>
2023-03-01 21:28:41 +0530

    Added programming languages support for

--- src/utils/iconUtils.ts
+++ src/utils/iconUtils.ts
@@ -1,14 +1,53 @@
-import { SiTypescript, SiJavascript, SiPython } from 'react-icons/si';
+import {
+  SiTypescript,
+  SiJavascript,
+  SiPython,
+  SiPhp,
+  SiReact,
+  SiHtml5,
+  SiCss3,
+  SiRuby,
+  SiSwift,
+  SiKotlin,
+  SiRust,
+  SiPerl,
+  SiMysql,
+  SiPostgresql,
+  SiDart,
+} from "react-icons/si";
 
 export const getIconByLanguage = (language: string) => {
   switch (language) {
-    case 'TypeScript':
+    case "TypeScript":
       return SiTypescript;
-    case 'JavaScript':
+    case "JavaScript":
       return SiJavascript;
-
-    case 'Python':
+    case "React":
+      return SiReact;
+    case "Python":
       return SiPython;
+    case "PHP":
+      return SiPhp;
+    case "HTML":
+      return SiHtml5;
+    case "CSS":
+      return SiCss3;
+    case "Ruby":
+      return SiRuby;
+    case "Swift":
+      return SiSwift;
+    case "Kotlin":
+      return SiKotlin;
+    case "Rust":
+      return SiRust;
+    case "Perl":
+      return SiPerl;
+    case "MySQL":
+      return SiMysql;
+    case "PostgreSQL":
+      return SiPostgresql;
+    case "Dart":
+      return SiDart;
 
     default:
       break;
